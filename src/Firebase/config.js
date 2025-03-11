a15ca533df1d19d95c84851139125b063841ed72
Mscode04 <ms.shaheenkp@gmail.com>
2025-03-11 13:11:15 +0530

    app-updated2.01

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvg80SUhF_bYfqxGrI9jeG3jupTFQSKH0",
-  authDomain: "palliative-app-version-1.firebaseapp.com",
-  projectId: "palliative-app-version-1",
-  storageBucket: "palliative-app-version-1.firebasestorage.app",
-  messagingSenderId: "606406902443",
-  appId: "1:606406902443:web:02076908304fd0f65e2dee",
-  measurementId: "G-BKED092BZT"
+  apiKey: "AIzaSyCxPIrwvFUU-2Cayh_C_ROC6ipRU_tqU9U",
+  authDomain: "palliative-app-10572.firebaseapp.com",
+  projectId: "palliative-app-10572",
+  storageBucket: "palliative-app-10572.firebasestorage.app",
+  messagingSenderId: "881815533939",
+  appId: "1:881815533939:web:c4d123ef491a87d0966d06",
+  measurementId: "G-633L797DCK"
 };
 
 const FirebaseApp = initializeApp(firebaseConfig);
